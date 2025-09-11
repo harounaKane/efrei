5b1f9f51d0f12c04846cd45db611b9fab58daf9b
harounaKane <harouna.Kane@hotmail.fr>
2025-09-11 10:40:51 +0200

    corrige tp jsonplaceholder

--- API/api.js
+++ API/api.js
@@ -1,20 +1,85 @@
 const qs = (el) => document.querySelector(el);
-const API = "https://jsonplaceholder.typicode.com/users";
+const qa = (el) => document.querySelectorAll(el);
+const API = "https://jsonplaceholder.typicode.com";
 
-const user = qs("#users");
+const userContent = qs("#users");
+const postContent = qs(".postContent");
 
 async function getUsers(){
-    const res = await fetch("https://jsonplaceholder.typicode.com/users"); 
-    if( !res.ok ) throw new Error("Erreur: " + res.status);
-    const users = await res.json();
+    try{
+        const res = await fetch(`${API}/users`); 
+        if( !res.ok ) throw new Error("Erreur: " + res.status);
+        const users = await res.json();
 
-    let textuser = '';
-    users.map( user => {
-        textuser += `<div class="user">${user.name}</div>`
-    });
+        users.forEach( user => {
+            const div = document.createElement("div");
+            div.className = "user";
+            div.textContent = user.name;
+            
+            userContent.appendChild(div);
 
-    user.innerHTML = textuser;
-  
+            div.addEventListener("click", () => {
+                qa(".user").forEach( u => u.classList.remove("selected"));
+                div.classList.add("selected");
+
+                userPosts(user);
+            });        
+        });
+    }catch(e){userContent.innerHTML = `Erreur chargement user : ${e}`}
+}
+
+
+async function userPosts(user) {
+    console.log(user);
+    qs("#user").innerHTML = user.name;
+
+    try{
+        const res = await fetch(`${API}/posts?userId=${user.id}`);
+        if( !res.ok ) throw new Error("Erreur HTTP : " + res.status);
+        const posts = await res.json();
+
+        postContent.innerHTML = "";
+        if( posts.length == 0 ){postContent.innerHTML = "Pas de postes"; return; }
+        qs("#user").innerHTML = user.name;
+
+        posts
+        .sort( (a, b) => a.title.localeCompare(b.title) )
+        .forEach( p => {
+            const div = document.createElement("div");
+            div.className = "post";
+            div.innerHTML = `<h3>${p.title}</h3><p>${p.body}</p>`;
+
+            postContent.appendChild(div);
+
+            div.addEventListener("click", () => postComments(user, p));
+        });
+        
+    }catch(e){
+        postContent.innerHTML = "Erreur :" + e;
+    }
+}
+
+async function postComments(user, post) {
+    try{
+        const res = await fetch(`${API}/comments?postId=${post.id}`);
+        if( !res.ok ) throw new Error("Erreur http: " + res.status);
+
+        const comments = await res.json();
+        
+        postContent.innerHTML = "";
+        qs("#post").innerHTML = `${post.title}<p>Auteur : ${user.name}</p>`;
+        if( comments.length == 0 ){postContent.innerHTML = "Pas de postes"; return; }
+
+        comments.forEach( c => {
+            const div = document.createElement("div");
+            div.className = "post";
+            div.innerHTML = `<h4>${c.name}</h4><p>${c.body}</p>`;
+
+            postContent.appendChild(div);
+        });
+
+    }catch(e){}
+    
 }
 
 getUsers();
